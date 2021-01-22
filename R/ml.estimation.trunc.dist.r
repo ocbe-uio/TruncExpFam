0d398e6566719d05f91353e224f03b2dabd3df2b
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-01-22 08:27:27 +0100

    Improved examples (#4)

--- R/ml.estimation.trunc.dist.r
+++ R/ml.estimation.trunc.dist.r
@@ -17,26 +17,30 @@
 #' # Normal
 #' sample.norm <- rtrunc(n = 10000, mu = 2, sigma = 1.5, a = -1)@sample
 #' ml.estimation.trunc.dist(
-#'   sample.norm, y.min = -1, max.it = 500, delta = 0.33, family = "Gaussian"
+#'   sample.norm, y.min = -1, max.it = 500, delta = 0.33, family = "Gaussian",
+#'   print.iter = TRUE
 #' )
 #'
 #' # Log-Normal
 #' sample.lognorm <- rtrunc(n = 100000, mulog = 2.5, sigmalog = 0.5, a = 7)@sample
 #' ml_lognormal <- ml.estimation.trunc.dist(
 #'   sample.lognorm, y.min = 7, max.it = 500, tol = 1e-10, delta = 0.3,
-#'   family = "LogNormal"
+#'   family = "LogNormal", print.iter = FALSE
 #' )
+#' ml_lognormal
 #'
 #' # Poisson
 #' sample.pois <- rtrunc(n=1000, lambda=10, a=4)@sample
 #' ml.estimation.trunc.dist(
-#'   sample.pois, y.min = 4, max.it = 500, delta = 0.33, family = "Poisson"
+#'   sample.pois, y.min = 4, max.it = 500, delta = 0.33, family = "Poisson",
+#'   print.iter = 5
 #' )
 #'
 #' # Gamma
 #' sample.gamma <- rtrunc(n = 10000, alpha = 6, beta = 2, a = 2)@sample
 #' ml.estimation.trunc.dist(
-#'   sample.gamma, y.min = 2, max.it = 1500, delta = 0.3, family = "Gamma"
+#'   sample.gamma, y.min = 2, max.it = 1500, delta = 0.3, family = "Gamma",
+#'   print.iter = 10
 #' )
 #' @export
 # TODO: add option to suppress output
