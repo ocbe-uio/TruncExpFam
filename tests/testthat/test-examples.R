03716ad33087d476bc9ac909d93826b7801be8a0
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-01-29 09:29:52 +0100

    Improved tests

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -20,43 +20,32 @@ sample.gamma <- rtrunc(n = 10000, alpha = 6, beta = 2, a = 2)
 # lines(y, density.trunc.lognorm(y, eta = c(10, -2), a = 7), lwd = 2, col = 2)
 # hist(log(sample.lognorm))
 
-test_that("rtrunc.norm works", {
+test_that("rtrunc samples have the expected values", {
+	tol <- 1e-3
 	expect_equal(head(sample.norm, 3), c(2.8645444, 2.1329365, 3.0388173))
-})
-test_that("rtrunc.pois works", {
+	expect_equal(head(sample.lognorm, 3), c(16.2514, 12.7345, 17.2235), tol=tol)
 	expect_equal(head(sample.pois), c(11, 10, 12, 18, 12, 11))
-})
-test_that("rtrunc.gamma works", {
+	expect_equal(head(sample.binom), c(10, 10, 9, 10, 8, 9))
 	expect_equal(head(sample.gamma, 3), c(3.4673, 2.8549, 3.6220), tol = 1e-4)
 })
 
-context("Matching output of stats::r*")
-
-test_that("stats::rnorm", {
+test_that("Output of rtrunc matches stats::r*", {
 	expect_setequal(
 		object   = {set.seed(1); rnorm(500, mean=1, sd=3)},
 		expected = {set.seed(1); rtrunc(500, mu=1, sigma=3)}
 	)
-})
-test_that("stats::rbinom", {
 	expect_setequal(
 		object   = {set.seed(1); rbinom(500, size=10, prob=.3)},
 		expected = {set.seed(1); rtrunc(500, size=10, prob=.3)}
 	)
-})
-test_that("stats::rgamma", {
 	expect_setequal(
 		object   = {set.seed(1); rgamma(500, shape=4, rate=5)},
 		expected = {set.seed(1); rtrunc(500, alpha=4, beta=5)}
 	)
-})
-test_that("stats::rlnorm", {
 	expect_setequal(
 		object   = {set.seed(1); rlnorm(500, meanlog=7, sdlog=2)},
 		expected = {set.seed(1); rtrunc(500, mulog=7, sigmalog=2)}
 	)
-})
-test_that("stats::rpois", {
 	expect_setequal(
 		object   = {set.seed(1); rpois(500, lambda=72)},
 		expected = {set.seed(1); rtrunc(500, lambda=72)}
@@ -87,7 +76,7 @@ ml_poisson <- ml.estimation.trunc.dist(
 	sample.pois, y.min = 4, max.it = 500, delta = 0.33, family = "Poisson",
 	print.iter = FALSE
 )
-# FIXME: ml.estimation.trunc.dist not working for binomial
+# FIXME: ml.estimation.trunc.dist not working for binomial (#19)
 # ml_binom <- ml.estimation.trunc.dist(
 # 	sample.binom, y.min = 4, max.it = 500, delta = 0.33, family = "Binomial",
 # 	nsize = 10, print.iter = FALSE
@@ -97,18 +86,19 @@ ml_gamma <- ml.estimation.trunc.dist(
 	print.iter = FALSE
 )
 
-test_that("ML estimation for binomial works", {
-
-})
 test_that("ml.estimation.trunc.dist works", {
 	expect_equal(ml_gaussian, c(mu = 2.041146, sd = 1.481114), tol = 1e-6)
 	expect_equal(ml_lognormal, c(mu = 2.5207512, sd = 0.4842092), tol = 1e-6)
-	expect_equivalent(ml_poisson, 10.18402, tol = 1e-5) # TODO: name output lambda
+	expect_equal(ml_poisson, c(lambda = 10.18402), tol = 1e-5)
+	# TODO: add unit test for ml_binom (depends on #19)
+	expect_equal(ml_gamma, c(alpha = 6.520333, beta = 2.109503), tol = 1e-5)
 })
 
 # ======================================================== #
 # Parameter conversion                                     #
 # ======================================================== #
+
+context("Parameter conversion")
 
 eta.hat <- parameters2natural.gamma(ml_lognormal)
 
