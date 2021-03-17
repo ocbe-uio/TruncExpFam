101297670292184220313657e08cb7d3afdd4f74
Waldir Leoncio <waldir.leoncio@gmail.com>
2021-03-17 10:11:34 +0100

    Added other distros to validation function

--- R/genRtruncClass.R
+++ R/genRtruncClass.R
@@ -3,12 +3,19 @@
 #' @param ... parameter list passed to `rtrunc`
 #' @return A character string.
 #' @author Waldir Leoncio
-#' @export
 genRtruncClass <- function(n, family, parms) {
-	validateFamilyParms(family, parms)
+
+	# Dropping a and b (parameters not used for validating) -- #
+	parms <- parms[!(parms %in% c("a", "b"))]
+
+	# Validating --------------------------------------------- #
+	is_family_parms_valid <- validateFamilyParms(family, parms)
+	if (is_family_parms_valid) {
+		return(family)
+	}
 }
 
-validateFamilyParms <- function(family, parms) {
+validateFamilyParms <- function(family, parms, verbose=FALSE) {
 	valid_fam_parm <- list(
 		normal = list(
 			family = c("gaussian", "normal"),
@@ -17,18 +24,38 @@ validateFamilyParms <- function(family, parms) {
 		poisson = list(
 			family = c("poisson"),
 			parms  = c("lambda")
+		),
+		binomial = list(
+			family = c("binomial"),
+			parms  = c("size", "prob")
+		),
+		gamma = list(
+			family = c("gamma"),
+			parms  = c("shape", "rate")
+		),
+		lognormal = list(
+			family = c("log-normal", "lognormal"),
+			parms  = c("meanlog", "sdlog")
+		),
+		contbernoulli = list(
+			family = c("contbernoulli"),
+			parms  = c("lambda")
+		),
+		chisq = list(
+			family = c("chisq"),
+			parms  = c("df")
 		)
 	)
 	matched <- list(family = FALSE, parameters = FALSE)
 	for (fam in names(valid_fam_parm)) {
 		if (any(family == valid_fam_parm[[fam]]$family)) {
-			message("Matched family: ", family)
+			if(verbose) message("Matched family: ", family)
 			matched$family <- TRUE
 			parms_text <- paste(parms, collapse=", ")
 			parms_expected <- valid_fam_parm[[fam]]$parms
 			parms_expected_text <- paste(parms_expected, collapse=", ")
 			if (all(sort(parms) == sort(parms_expected))) {
-				message("Matched parameters: ", parms_text)
+				if (verbose) message("Matched parameters: ", parms_text)
 				matched$parameters <- TRUE
 			} else {
 				stop(
