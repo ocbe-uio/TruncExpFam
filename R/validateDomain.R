0a98c259365f1cb236eabdb93b7d5c9a6eae4c99
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-13 15:29:15 +0200

    Added domain validation to rtrunc()

---
+++ R/validateDomain.R
@@ -0,0 +1,51 @@
+validateDomain <- function(n, ...) {
+	parms <- list(...)
+	UseMethod("validateDomain")
+}
+
+validateDomain.normal <- function(n, mean, sd, ...) {
+	if (is.complex(mean)) stop("Invalid domain. mean must be real.")
+	if (sd <= 0) stop("Invalid domain. sd must be > 0.")
+}
+
+validateDomain.poisson <- function(n, lambda, ...) {
+	if (lambda != as.integer(lambda) | lambda < 0) {
+		stop("Invalid domain. lambda must be a natural number.")
+	}
+}
+
+validateDomain.binomial <- function(n, size, prob, ...) {
+	if (size != as.integer(size) | size < 0) {
+		stop("Invalid domain. size must be a natural number.")
+	}
+	if (prob < 0 | prob > 1) {
+		stop("Invalid domain. prob must be [0, 1].")
+	}
+}
+
+validateDomain.gamma <- function(n, shape, rate, ...) {
+	if (shape <= 0) stop("Invalid domain. shape must be > 0.")
+	if (rate <= 0) stop("Invalid domain. rate must be > 0.")
+}
+
+validateDomain.lognormal <- function(n, meanlog, sdlog, ...) {
+	if (is.complex(meanlog)) stop("Invalid domain. meanlog must be real.")
+	if (sdlog <= 0) stop("Invalid domain. rate must be > 0.")
+}
+
+validateDomain.contbernoulli <- function(n, meanlog, sdlog, ...) {
+	if (is.complex(meanlog)) stop("Invalid domain. meanlog must be real.")
+	if (sdlog <= 0) stop("Invalid domain. rate must be > 0.")
+}
+
+validateDomain.contbernoulli <- function(n, lambda, ...) {
+	if (lambda <= 0 | lambda >= 1) {
+		stop("Invalid domain. lambda must be a (0, 1).")
+	}
+}
+
+validateDomain.chisq <- function(n, df, ...) {
+	if (df != as.integer(df) | df < 0) {
+		stop("Invalid domain. df must be a natural positive number.")
+	}
+}
