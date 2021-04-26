69445f856e6db74b77e3c4b4a65229bcee2f6d60
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-26 11:05:15 +0200

    Moved dtrunc() to genericFunctions.R

--- R/genericFunctions.R
+++ R/genericFunctions.R
@@ -1,3 +1,16 @@
+#' @title Probability Density Function
+#' @description Calculates the PDF for a given truncated distribution
+#' @param y output from rtrunc
+#' @param ... arguments to be passed to the methods
+#' @export
+#' @examples
+#' y <- rtrunc(50, mean=5, sd=2)
+#' dtrunc(y, eta=c(0, -1))
+#'
+dtrunc <- function(y, ...) {
+	UseMethod("dtrunc", y)
+}
+
 #' @title Initialize parameters
 #' @description Returns the empirical parameter estimate for a distribution
 #' @param y output of rtrunc
