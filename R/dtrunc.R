9a69605630b6c7dfaa73c887db2a72caecc799cd
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-26 10:45:57 +0200

    Adding example for dtrunc()

--- R/dtrunc.R
+++ R/dtrunc.R
@@ -3,6 +3,10 @@
 #' @param y output from rtrunc
 #' @param ... arguments to be passed to the methods
 #' @export
+#' @examples
+#' y <- rtrunc(50, mean=5, sd=2)
+#' dtrunc(y, eta=c(0, -1))
+#'
 dtrunc <- function(y, ...) {
-	UseMethods("dtrunc", y)
+	UseMethod("dtrunc", y)
 }
