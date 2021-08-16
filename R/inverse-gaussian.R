0971fc4dee650fd305122501463bac285e86e408
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-08-16 16:24:22 +0200

    Syntax fixes (#47)

--- R/inverse-gaussian.R
+++ R/inverse-gaussian.R
@@ -22,21 +22,8 @@ rtruncinvgauss <- rtrunc.invgauss <- function(n, m, s, a=0, b=Inf) {
 #' @importFrom rmutil rinvgauss
 #' @rdname dtrunc
 #' @export
-dtruncinvgauss <- rtrunc.invgauss <- function(n, m, s, a=0, b=Inf) {
-  y <- rinvgauss(n, m, s)
-  if (!missing(a)) {
-	y <- y[y >= a]
-  }
-  if (!missing(b)) {
-	y <- y[y <= b]
-  }
-  class(y) <- "trunc_invgauss"
-  return(y)
-}
-
-#' @export
 #' @importFrom rmutil dinvgauss pinvgauss
-dtrunc.trunc_invgauss <- function(y, eta, a = -Inf, b = Inf) {
+dtruncinvgauss <- dtrunc.trunc_invgauss <- function(y, eta, a = -Inf, b = Inf) {
 	parm <- natural2parameters.trunc_invgauss(eta)
 	dens <- ifelse((y < a) | (y > b), 0, dinvgauss(y, m = parm[1], s = parm[2]))
 	if (!missing(a)) {
