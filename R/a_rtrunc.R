6b896fc7600be79404b1d74ea59136a3155ed0d3
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-09-06 11:54:53 +0200

    Moved genRtruncClass.R functions to a_rtrunc.R

--- R/a_rtrunc.R
+++ R/a_rtrunc.R
@@ -80,3 +80,52 @@ rtrunc <- function(n, family="gaussian", ...) {
 rtrunc.generic <- function(n, ...) {
 	UseMethod("rtrunc", n)
 }
+
+#' @title Generates an rtrunc-dispatchable class
+#' @description Matches a list of arguments to an rtrunc method
+#' @param n sample size
+#' @param family distribution family
+#' @param parms list of parameters passed to rtrunc (through the `...` element)
+#' @return A character string.
+#' @author Waldir Leoncio
+genRtruncClass <- function(n, family, parms) {
+
+	# Dropping a and b (parameters not used for validating) -- #
+	parms <- parms[!(parms %in% c("a", "b"))]
+
+	# Validating --------------------------------------------- #
+	validation_family_parms <- validateFamilyParms(family, parms)
+	if (validation_family_parms$is_valid) {
+		family <- validation_family_parms$family_name
+		return(family)
+	}
+}
+
+validateFamilyParms <- function(family, parms, verbose=FALSE) {
+	matched <- list(family = FALSE, parameters = FALSE)
+	families <- grep(family, names(valid_fam_parm))
+	for (fam in families) {
+		if (any(family == valid_fam_parm[[fam]]$family)) {
+			if(verbose) message("Matched family: ", family)
+			matched$family <- TRUE
+			family <- valid_fam_parm[[fam]]$family[1] # use standard family name
+			parms_text <- paste(parms, collapse=", ")
+			parms_expected <- valid_fam_parm[[fam]]$parms
+			if (all(sort(parms) == sort(parms_expected))) {
+				if (verbose) message("Matched parameters: ", parms_text)
+				matched$parameters <- TRUE
+			}
+		}
+	}
+	if (!matched$parameters) {
+		parms_expected_text <- paste(unlist(parms_expected), collapse=", ")
+		stop(
+			"The {", parms_text, "} ",
+			"parameter set does not match the ", family, " family. ",
+			"Expected set of parameters: {", parms_expected_text, "}. ",
+			"Please change the family to match the expected ",
+			"parameters or use a different family."
+		)
+	}
+	return(list(is_valid = all(unlist(matched)), family_name = family))
+}
