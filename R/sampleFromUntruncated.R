f258bdf6bae68eed3b9f00d5702ae5d5bf5b74e3
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-12-13 10:38:15 +0100

    Fixed sample size for aliases

--- R/sampleFromUntruncated.R
+++ R/sampleFromUntruncated.R
@@ -30,10 +30,33 @@ sampleFromTruncated <- function(parms) {
     "trunc_normal" = rnorm(n, mean, sd),
     "trunc_poisson" = rpois(n, lambda)
   ))
-
-  # Truncating -----------------------------------------------------------------
   y <- y[y >= parms$a]
   y <- y[y <= parms$b]
+
+  extra_n <- 1 # to generate extra observations to complete n from input
+	while (length(y) != parms$n) {
+    new_y <- with(parms, switch(class(n),
+      "trunc_beta" = rbeta(extra_n, shape1, shape2),
+      "trunc_binomial" = rbinom(extra_n, size, prob),
+      "trunc_chisq" = rchisq(extra_n, df),
+      "trunc_contbern" = rcontbern(extra_n, lambda),
+      "trunc_exp" = rexp(extra_n, rate),
+      "trunc_gamma" = rgamma(extra_n, shape = shape, scale = scale),
+      "trunc_invgamma" = rinvgamma(extra_n, shape = shape, scale = scale),
+      "trunc_invgauss" = rinvgauss(extra_n, m, s),
+      "trunc_lognormal" = rlnorm(extra_n, meanlog, sdlog),
+      "trunc_nbinom" = rnbinom(extra_n, size, prob, mu),
+      "trunc_normal" = rnorm(extra_n, mean, sd),
+      "trunc_poisson" = rpois(extra_n, lambda)
+    ))
+
+    # Truncating ---------------------------------------------------------------
+    new_y <- new_y[new_y >= parms$a]
+    new_y <- new_y[new_y <= parms$b]
+
+    # Assembling ---------------------------------------------------------------
+		y <- append(y, new_y)
+	}
 
   # Attaching attributes -------------------------------------------------------
   class(y) <- className
