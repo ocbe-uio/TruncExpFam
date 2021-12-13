a7c38a75d27c7d90e5e486afd7d9c4cc883a18fc
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-12-13 09:21:21 +0100

    Separated function into its own file

--- R/a_rtrunc.R
+++ R/a_rtrunc.R
@@ -149,13 +149,3 @@ validateFamilyParms <- function(family, parms, verbose=FALSE) {
 	}
 	return(list(is_valid = all(unlist(matched)), family_name = family))
 }
-
-attachDistroAttributes <- function(sample, family, parms) {
-	if (length(attributes(sample)) == 1) {
-		family_attributes <- valid_fam_parm[[family]]
-		attr(sample, "parameters") <- parms[family_attributes$parms]
-		attr(sample, "truncation_limits") <- parms[c("a", "b")]
-		attr(sample, "continuous") <- family_attributes$cont
-	}
-	return(sample)
-}
