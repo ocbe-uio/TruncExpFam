5ea198303b3d0cba4b6c4891ed401f7db4342d0a
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-11-24 15:05:49 +0100

    Added simple tests for estimation (#95)

--- tests/testthat/test-estimating-from-classless.R
+++ tests/testthat/test-estimating-from-classless.R
@@ -2,15 +2,31 @@ context("Estimating from numeric (classless) vectors")
 
 set.seed(2828324)
 
-x <- rnorm(1e4, mean = 9, sd = 2)
-y <- rbeta(1e4, shape1 = 9, shape2 = 2)
-# TODO: add more as needed to test all distros
+cont01 <- rbeta(1e4, shape1 = 10, shape2 = 100)
+cont01b <- cont01 / max(cont01)  # better resembles a contbern candidate
+disc0inf <- rpois(1e4, lambda = 10)
+cont0inf <- rchisq(1e4, df = 10)
+cont0infb <- exp(-cont0inf)
+contReal <- rnorm(1e4, mean = 10, sd = 10)
 
 test_that("Estimation works, in general", {
-  expect_error(mlEstimationTruncDist(x), "choose an underlying distribution")
-  expect_named(mlEstimationTruncDist(x, family = "normal"), c("mean", "sd"))
-  expect_named(mlEstimationTruncDist(x, family = "gamma"), c("shape", "rate"))
-  expect_error(mlEstimationTruncDist(x, family = "beta"), "outside of support")
+  # Improper samples
+  expect_error(mlEstimationTruncDist(contReal), "choose an underlying distribution")
+  expect_error(mlEstimationTruncDist(contReal, family = "beta"), "outside of support")
+
+  # Proper samples
+  expect_named(mlEstimationTruncDist(cont01, family = "beta"), c("shape1", "shape2"))
+  expect_named(mlEstimationTruncDist(disc0inf, family = "binomial"), c("prob"))
+  expect_named(mlEstimationTruncDist(cont0inf, family = "chisq"), c("df"))
+  expect_named(mlEstimationTruncDist(cont01b, family = "contbern"), c("lambda"))
+  expect_named(mlEstimationTruncDist(cont0infb, family = "exp"), c("rate"))
+  expect_named(mlEstimationTruncDist(cont0inf, family = "gamma"), c("shape", "rate"))
+  expect_named(mlEstimationTruncDist(cont0inf, family = "invgamma"), c("shape", "rate"))
+  expect_named(mlEstimationTruncDist(cont0inf, family = "invgauss", tol = .1), c("m", "s"))
+  expect_named(mlEstimationTruncDist(cont0inf, family = "lognormal"), c("mean", "sd"))
+  expect_named(mlEstimationTruncDist(disc0inf, family = "nbinom"), c("mean"))
+  expect_named(mlEstimationTruncDist(contReal, family = "normal"), c("mean", "sd"))
+  expect_named(mlEstimationTruncDist(disc0inf, family = "poisson"), c("lambda"))
 })
 
 test_that("Original parameters are retrieved", {
