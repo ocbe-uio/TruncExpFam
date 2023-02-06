410e9767d925b4f1bb15442596e632af1a991759
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-02-06 14:40:28 +0100

    Fixed global assignment of parms (#78)

--- R/rtrunc_direct.R
+++ R/rtrunc_direct.R
@@ -21,7 +21,8 @@ rtrunc_direct.normal <- function(n, family, mean = 0, sd = 1, a = -Inf, b = Inf,
   F_b <- cumDens(b, pnorm, mean, sd)
   q_T <- qnorm(runif(n) * (F_b - F_a) + F_a, mean, sd)
   class(q_T) <- paste0("trunc_", class(n))
-  q_T <- attachDistroAttributes(q_T, class(n), c(parms, "a" = a, "b" = b)) # FIXME: parms is global
+  parms <- list(...)
+  q_T <- attachDistroAttributes(q_T, class(n), c(parms, "a" = a, "b" = b))
   return(q_T)
 }
 
@@ -32,7 +33,8 @@ rtrunc_direct.beta <- function(n, family, shape1, shape2, a = 0, b = 1, ...) {
   F_b <- cumDens(b, pbeta, shape1, shape2)
   q_T <- qbeta(runif(n) * (F_b - F_a) + F_a, shape1, shape2)
   class(q_T) <- paste0("trunc_", class(n))
-  q_T <- attachDistroAttributes(q_T, class(n), c(parms, "a" = a, "b" = b)) # FIXME: parms is global
+  parms <- list(...)
+  q_T <- attachDistroAttributes(q_T, class(n), c(list, "a" = a, "b" = b))
   return(q_T)
 }
 
