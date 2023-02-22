1f1cf6191ff919280022eab993080820fa0618b0
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-02-22 14:19:06 +0100

    Added function to rescale quantiles (#78)
    Function is similar to `rescaledDensities()`, though the latter is
    supposed to serve #77 (i.e., discrete distributions). Some eventual
    merging may be in order.

--- R/rtrunc_direct.R
+++ R/rtrunc_direct.R
@@ -19,7 +19,7 @@ rtrunc_direct <- function(n, family = "gaussian", ...) {
 rtrunc_direct.normal <- function(n, family, mean = 0, sd = 1, a = -Inf, b = Inf, ...) {
   F_a <- cumDens(a, pnorm, mean, sd)
   F_b <- cumDens(b, pnorm, mean, sd)
-  q_T <- truncated_q(qnorm(runif(n) * (F_b - F_a) + F_a, mean, sd), mget(ls()))
+  q_T <- truncated_q(qnorm(rescaled_q(n, F_a, F_b), mean, sd), mget(ls()))
   return(q_T)
 }
 
@@ -28,7 +28,7 @@ rtrunc_direct.normal <- function(n, family, mean = 0, sd = 1, a = -Inf, b = Inf,
 rtrunc_direct.beta <- function(n, family, shape1, shape2, a = 0, b = 1, ...) {
   F_a <- cumDens(a, pbeta, shape1, shape2)
   F_b <- cumDens(b, pbeta, shape1, shape2)
-  q_T <- truncated_q(qbeta(runif(n) * (F_b - F_a) + F_a, shape1, shape2), mget(ls()))
+  q_T <- truncated_q(qbeta(rescaled_q(n, F_a, F_b), shape1, shape2), mget(ls()))
   return(q_T)
 }
 
@@ -37,7 +37,7 @@ rtrunc_direct.beta <- function(n, family, shape1, shape2, a = 0, b = 1, ...) {
 rtrunc_direct.chisq <- function(n, family, df, a = 0, b = Inf, ...) {
   F_a <- cumDens(a, pchisq, df)
   F_b <- cumDens(b, pchisq, df)
-  q_T <- truncated_q(qchisq(runif(n) * (F_b - F_a) + F_a, df), mget(ls()))
+  q_T <- truncated_q(qchisq(rescaled_q(n, F_a, F_b), df), mget(ls()))
   return(q_T)
 }
 
@@ -45,7 +45,7 @@ rtrunc_direct.chisq <- function(n, family, df, a = 0, b = Inf, ...) {
 rtrunc_direct.contbern <- function(n, family, lambda, a = 0, b = 1, ...) {
   F_a <- cumDens(a, pcontbern, lambda)
   F_b <- cumDens(b, pcontbern, lambda)
-  q_T <- truncated_q(qcontbern(runif(n) * (F_b - F_a) + F_a, lambda), mget(ls()))
+  q_T <- truncated_q(qcontbern(rescaled_q(n, F_a, F_b), lambda), mget(ls()))
   return(q_T)
 }
 
@@ -67,4 +67,8 @@ truncated_q <- function(q_T, parms) {
     parms  = c(parms$parms, parms["a"], parms["b"])
   )
   return(q_T)
+}
+
+rescaled_q <- function(n, F_a, F_b) {
+  return(runif(n) * (F_b - F_a) + F_a)
 }
