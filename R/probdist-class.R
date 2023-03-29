a9bcaed97eea8ee6b1092c92dcd66e43ae76dbe8
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-03-29 09:52:20 +0200

    Deprecated is_natural (#88)
    Natural parameters are detected by the name of the parameters passed (they must begin with "eta")

--- R/probdist-class.R
+++ R/probdist-class.R
@@ -8,13 +8,20 @@ probdist <- setRefClass(
 )
 
 probdist$methods(
-  initialize = function(..., family, is_natural = FALSE) {
-    # Validation ---------------------------------------------------------------
+  initialize = function(..., family) {
+    # Retrieving parameters and detecting if they are natural ------------------
     parms <<- c(...)
+    is_natural <- all(substr(names(parms), 1, 3) == "eta")
+
+    # Validation ---------------------------------------------------------------
     family <<- useStandardFamilyName(family)
-    validateFamilyParms(.self$family, names(parms))
-    valDomFun <- get(paste0("validateDomain.trunc_", .self$family))
-    valDomFun(NA, as.list(parms))
+    if (!is_natural) {
+      validateFamilyParms(.self$family, names(parms))
+      valDomFun <- get(paste0("validateDomain.trunc_", .self$family))
+      valDomFun(NA, as.list(parms))
+    }
+
+    # Converting parameters ----------------------------------------------------
     if (is_natural) {
       nat_parms <<- parms
       convFun <- get(paste0("natural2parameters.trunc_", .self$family))
