8b27c6112e07425f6ad621a5e8a8b65f260c20af
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-09-22 15:46:13 +0200

    Removed unused `psi()` function

--- R/logGammaFunction.R
+++ R/logGammaFunction.R
@@ -2,14 +2,7 @@
 # Evaluating the derivative of log(Gamma(x))
 # See https://www.nbi.dk/~polesen/borel/node5.html
 # C:  Euler-Mascheroni Constant.
-psi <- function(x, k = 10000) {
-  # OBS: This function is not used!
-  sq <- 1:10000000
-  C <- sum(1/sq)-log(max(sq))
-  -C + sum(1 / (1:k) - 1 / ((1:k) + x - 1))
-}
-
 dpsi.dx <- function(x, k = 10000) {
-  # Returns the derivative of the psi function above
+  # Returns the derivative of the psi function (removed from above)
   sum((1 / ((0:k) + x))^2)
 }
