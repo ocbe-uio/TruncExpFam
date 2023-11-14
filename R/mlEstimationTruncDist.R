36297408069aebaa6ba961652f82b58374699006
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-11-14 13:15:25 +0100

    Replaced "delta" with "delta.L2" in output (#94)

--- R/mlEstimationTruncDist.R
+++ R/mlEstimationTruncDist.R
@@ -108,7 +108,7 @@ mlEstimationTruncDist <- function(y, y.min = attr(y, "truncation_limits")$a,
       if (it %% as.numeric(print.iter) == 0) {
         cat(
           "it:", formatC(it, width = floor(log(max.it, 10))),
-          "delta:", formatC(delta.L2, width = 10),
+          "delta.L2:", formatC(delta.L2, width = 10),
           "parm:", round(parm.j, 3), "\n"
         )
       }
