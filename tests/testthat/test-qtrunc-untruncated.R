b2cece05eeb9c3963cd6a7e460b01398ea7f0562
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-07-03 11:56:35 +0200

    Adjusted unit tests (#54)

--- tests/testthat/test-qtrunc-untruncated.R
+++ tests/testthat/test-qtrunc-untruncated.R
@@ -8,13 +8,20 @@ test_that("qtrunc() works as expected (normal)", {
         sg <- rchisq(1L, 5L)
         pt <- runif(i)
         if (lg) pt <- log(pt)
-        q_trunc <- qtrunc(pt, "normal", mean = mn, sd = sg, lower.tail = lt, log.p = lg)
+        q_trunc <- qtrunc(
+          pt, "normal", mean = mn, sd = sg, lower.tail = lt, log.p = lg
+        )
         q_norm <- qnorm(pt, mean = mn, sd = sg, lower.tail = lt, log.p = lg)
         expect_length(pt, i)
         expect_length(q_trunc, i)
         for (ii in seq_along(pt)) {
           expect_equal(q_trunc[ii], q_norm[ii])
-          expect_equal(pt[ii], ptrunc(q_trunc[ii], "normal", mean = mn, sd = sg, lower.tail = lt, log.p = lg))
+          # Working back to p from q
+          ptr <- ptrunc(
+            q_trunc[ii], "normal", mean = mn, sd = sg,
+            lower.tail = lt, log.p = lg
+          )
+          expect_equal(pt[ii], ptr)
         }
       }
     }
