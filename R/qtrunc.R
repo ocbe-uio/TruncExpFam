80672d70cf934d29102e7f0af499dd5f671b7ca7
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-07-03 11:19:34 +0200

    Added analytical solution (#54)
    Source: https://github.com/ocbe-uio/TruncExpFam/issues/54#issuecomment-2153501813

--- R/qtrunc.R
+++ R/qtrunc.R
@@ -34,29 +34,50 @@ qtrunc.generic <- function(p, ..., lower.tail, log.p) {
 }
 
 qtrunc.normal <- function(
-  p, mean = 0, sd = 1, a = -Inf, b = Inf, ..., lower.tail, log.p
+  p, mean = 0, sd = 1, a = -Inf, b = Inf, ..., lower.tail, log.p, numeric = FALSE
   ) {
-  # Implements a simple bisection algorithm to find the quantile
-  lower <- rep(ifelse(a == -Inf, -.Machine$double.xmax, a), length(p))
-  upper <- rep(ifelse(b == +Inf, +.Machine$double.xmax, b), length(p))
-  q <- rowMeans(cbind(lower, upper))
-  p_q <- ptrunc.normal(q, mean, sd, a, b, lower.tail = lower.tail, log.p = log.p)
-  tol <- abs(p_q - p)
-  for (tl in seq_along(p)) {
-    iter <- 0L
-    while (tol[tl] > 1e-10 && iter < 1e9) {
-      trigger <- ifelse(lower.tail, p_q[tl] < p[tl], p_q[tl] > p[tl])
-      if (trigger) {
-        lower[tl] <- q[tl]
-        q[tl] <- mean(c(q[tl], upper[tl]))
-      } else {
-        upper[tl] <- q[tl]
-        q[tl] <- mean(c(lower[tl], q[tl]))
+  if (numeric) {
+    # Implements a simple bisection algorithm to find the quantile
+    lower <- rep(ifelse(a == -Inf, -.Machine$double.xmax, a), length(p))
+    upper <- rep(ifelse(b == +Inf, +.Machine$double.xmax, b), length(p))
+    q <- rowMeans(cbind(lower, upper))
+    p_q <- ptrunc.normal(q, mean, sd, a, b, lower.tail = lower.tail, log.p = log.p)
+    tol <- abs(p_q - p)
+    for (tl in seq_along(p)) {
+      iter <- 0L
+      while (tol[tl] > 1e-10 && iter < 1e9) {
+        trigger <- ifelse(lower.tail, p_q[tl] < p[tl], p_q[tl] > p[tl])
+        if (trigger) {
+          lower[tl] <- q[tl]
+          q[tl] <- mean(c(q[tl], upper[tl]))
+        } else {
+          upper[tl] <- q[tl]
+          q[tl] <- mean(c(lower[tl], q[tl]))
+        }
+        p_q[tl] <- ptrunc.normal(
+          q[tl], mean, sd, a, b, lower.tail = lower.tail, log.p = log.p
+        )
+        tol[tl] <- abs(p_q[tl] - p[tl])
+        iter <- iter + 1L
       }
-      p_q[tl] <- ptrunc.normal(q[tl], mean, sd, a, b, lower.tail = lower.tail, log.p = log.p)
-      tol[tl] <- abs(p_q[tl] - p[tl])
-      iter <- iter + 1L
     }
+  } else {
+    F_a <- pnorm(a, mean, sd, lower.tail, FALSE)
+    F_b <- pnorm(b, mean, sd, lower.tail, FALSE)
+    rescaled_p <- rescale_p(p, F_a, F_b, lower.tail, log.p)
+    q <- qnorm(rescaled_p, mean, sd, lower.tail, FALSE)
   }
   return(q)
+}
+
+rescale_p <- function(p, F_a, F_b, lower.tail, log.p) {
+  if (log.p) {
+    p <- exp(p)
+  }
+  if (lower.tail) {
+    p <- p * F_b + (1 - p) * F_a
+  } else  {
+    p <- p * F_a + (1 - p) * F_b
+  }
+  return(p)
 }
